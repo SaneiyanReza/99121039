1e530644afe8b3893b77b1c9d75eba71cca2dfa9
Reza Saneiyan <saneiyan.rzw@gmail.com>
2023-10-27 22:30:13 +0330

    feat(main) : update season

--- Program/index.php
+++ Program/index.php
@@ -1,5 +1,12 @@
 <?php
-require_once('./functions.php'); 
+require_once('./functions.php');
+$connection = connect_to_mysql();
+$res = $connection->query("SHOW DATABASES;");
+while ($row = mysqli_fetch_assoc($res)) {
+    echo $row['Database'] . "\n";
+}
+dd($res);
+$number_of_hours = 4; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,12 +15,17 @@ require_once('./functions.php');
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>اولین درس برنامه نویسی وب</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+        <style>
+            .lesson{
+                cursor:pointer;
+            }
+        </style>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     </head>
     <?php
     require('./lessons.php');
     require('./profs.php');
-    dd($profs);
-     
+    require('./relations.php');
     ?>
     <body dir="rtl">
         <h1>برنامه ترم اول سال تحصیلی 401-402 مهندسی نرم افزار</h1>
@@ -39,14 +51,28 @@ require_once('./functions.php');
                 <?php for($cnt=0;$cnt<5;$cnt++){ ?>
                     <tr>
                         <td><?php echo($week_days[$cnt]) ?></td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
+                        <?php for($tcount = 0; $tcount<$number_of_hours;$tcount++) {
+                            $k = $cnt*$number_of_hours+$tcount;
+                            $cell =  get_lessons_in_this_time($profs_lessons, $lessons, $profs ,$k);
+                            ?>
+                            <?php if($cell) {?>
+                                <td class="lesson"><?php print $cell ?></td>
+                                <?php }else{ ?>
+                                <td><?php print $cell ?></td>
+                                <?php } ?>
+                        <?php  } ?>
                     </tr>
                 <?php } ?>
                 </tbody>
             </table>
         </div>
     </body>
+    <script>
+        $(document).ready(function(){
+            $('.lesson').on('click',function(){
+                var text = $(this).html();
+                alert(text);
+            })
+        })
+    </script>
 </html>
